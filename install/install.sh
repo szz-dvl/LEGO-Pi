afb11ec3b6dac1795daa3e8f61ee545bd2bdb0d9
szz-dvl <pi@rpi2.(none)>
2014-06-16 12:08:20 +0000

    inst script

--- install/install.sh
+++ install/install.sh
@@ -2,9 +2,44 @@
 
 # This script is the LEGO-Pi library installer.
 
+mydir=`pwd`
+distro=`cat /etc/*-release | grep NAME | grep -v PRETTY | awk -F= {'print $2'} | tr -d \"`
+mylib=/usr/lib/liblego.so
+deps=( gsl-bin libgsl0-dev gcc patch ctags git-core make )
+wpi_git=git://git.drogon.net/wiringPi
+wpi_patch="$mydir"/patch/wpi.patch
+lego_pi="$mydir"/../
+wpi="$mydir"/wpi/wiringPi
+wpi_ins="$mydir"/wpi/
+wpi_dev="$wpi_ins"/devLib
+wpi_gpio="$wpi_ins"/gpio
+pach_files=( wiringPi.c wiringPi.h softPwm.c softPwm.h )
+patch_dir="$mydir"/patch/files
+tmp_d="$mydir"/patch/tmp
+
+ndone="Nothing done, exiting"
+
+make_patch()
+{
+
+    #rm -r "$tmp_d"
+    mkdir "$tmp_d"
+    let "i=0"
+    rm "$wpi_patch"
+
+    for file in ${patch_files[@]}; do
+	diff -u "$wpi"/"$file" "$patch_dir"/"$file" > "$tmp_d"/p"$i".patch
+	echo "making patch "$i""
+	let "i=i+1"
+    done
+    
+    cat "$tmp_d"/p0.patch "$tmp_d"/p1.patch "$tmp_d"/p2.patch "$tmp_d"/p3.patch > "$wpi_patch"
+    rm -r "$tmp_d"
+}
+
 bye()
 {
-    echo ""$0": "$1""
+    echo ""$0": "$1"" >&2
     exit 1
 }
 
@@ -27,19 +62,7 @@ bold ()
 }
 
 
-mydir=`pwd`
-distro=`cat /etc/*-release | grep NAME | grep -v PRETTY | awk -F= {'print $2'} | tr -d \"`
-mylib=/usr/lib/liblego.so
-deps=(gsl-bin libgsl0-dev gcc patch ctags git-core)
-wpi_git=git://git.drogon.net/wiringPi
-wpi_patch="$mydir"/patch/wpi.patch
-lego_pi="$mydir"/../
-wpi="$mydir"/wpi/wiringPi
-wpi_ins="$mydir"/wpi/
-wpi_dev="$wpi_ins"/devLib
-wpi_gpio="$wpi_ins"/gpio
 
-ndone="Nothing done, exiting"
 
 if [ "$distro" == "Raspbian GNU/Linux" ]; then
     pkg_man=apt-get
@@ -55,7 +78,7 @@ min_clk=700
 # DEFAULTS:
 uninstall=false #install by default
 overclock=800   #little bit of overclocking by default 
-dvideo=true    #disable video outputs PAL/HDMI by default
+dvideo=true     #disable video outputs PAL/HDMI by default
 rmv_deps=false  #do not remove dependencies by when uninstalling default 
 
 while [[ $# -gt 0 ]]; do
@@ -97,12 +120,14 @@ if ! [[ "$uninstall" == "true" ]]; then
 
 
     if [ -d "$wpi_ins" ]; then
-	rm -r "$wpi_ins"
+	sudo rm -r "$wpi_ins"
     fi
 
     git clone "$wpi_git" "wpi"
+    make_patch
     cd "$wpi"
-    patch -N -R -p2 < "$wpi_patch"
+    patch -p2 < "$wpi_patch"
+    #patch -N -R -p2 < "$wpi_patch"
     cd "$wpi_ins"
     ./build
     
@@ -113,7 +138,9 @@ if ! [[ "$uninstall" == "true" ]]; then
     
     sudo make i2c && make && sudo make install && make test && make clean
     cd "$mydir"
-     
+
+    bold "\nLEGO_Pi successfully installed" ; echo "You will find the documentation in de the \"docs\" direcory"
+
 else #uninstall
     bold "\nUninstalling LEGO-Pi ..."
     
@@ -135,12 +162,13 @@ else #uninstall
 	sudo make uninstall
 	
 	cd "$mydir"
-	rm -r "$wpi_ins"
+	sudo rm -r "$wpi_ins"
 
     elif [ -e "$mylib" ]; then
 	bold "\nUnpatching wiringPi ..."
 	cd "$wpi"
-	patch -N -s -p2 < "$wpi_patch"
+	patch -R -p2 < "$wpi_patch"
+	#patch -N -s -p2 < "$wpi_patch"
 	cd "$wpi_ins"
 	./build
 	
@@ -151,8 +179,8 @@ else #uninstall
     
     cd "$mydir"
     
+    bold "\nLEGO_Pi successfully uninstalled"
     #Quitar seds, por lo menos de bashrc...
     
 fi
-
 exit 0
